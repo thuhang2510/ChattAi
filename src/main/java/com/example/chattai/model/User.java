32e3747b0e684d4e6e669568fa6ca2fcc633a05d
thuhang2510 <tunguyet2510@gmail.com>
2022-12-03 20:12:48 +0700

    add class User

---
+++ src/main/java/com/example/chattai/model/User.java
@@ -0,0 +1,27 @@
+package com.example.chattai.model;
+
+public class User {
+    private Integer id;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String password;
+    private String gender;
+
+    public User(String firstname, String lastname, String email, String password, String gender){
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.email = email;
+        this.password = password;
+        this.gender = gender;
+    }
+
+    public User(Integer id, String firstname, String lastname, String email, String password, String gender){
+        this.id = id;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.email = email;
+        this.password = password;
+        this.gender = gender;
+    }
+}
