9acbe3420f18ac5012a95596617c0afa5ac6df59
thuhang2510 <tunguyet2510@gmail.com>
2022-12-03 21:33:28 +0700

    add method processForgotPassword in class UserController

--- src/main/java/com/example/chattai/controllers/UserController.java
+++ src/main/java/com/example/chattai/controllers/UserController.java
@@ -5,8 +5,13 @@ import com.example.chattai.model.User;
 import com.example.chattai.request.LoginRequest;
 import com.example.chattai.respone.BasicRespone;
 import com.example.chattai.services.UserService;
+import com.example.chattai.utils.EmailUtil;
+import com.example.chattai.utils.Utility;
+import jakarta.mail.MessagingException;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
+import net.bytebuddy.utility.RandomString;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,11 +19,16 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.UnsupportedEncodingException;
+
 @RestController
 @RequestMapping("/api/v1")
 public class UserController {
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private EmailUtil emailUtil;
 
     @PostMapping("/login")
     public BasicRespone<String> getToken(@Valid @RequestBody LoginRequest loginRequest, BindingResult bindingResult, HttpSession httpSession) {
@@ -30,7 +40,7 @@ public class UserController {
             User user = userBasicRespone.getData();
 
             if (user == null)
-                return new BasicRespone<String>("User not exist", -1, null);
+                return new BasicRespone<String>(userBasicRespone.getMessage(), -1, null);
 
             httpSession.setAttribute("user", new UserDTO(user.getId(), user.getFirstname(), user.getLastname(), user.getGender(), user.getEmail()));
             return new BasicRespone<String>("Get token success", 0, httpSession.getId());
@@ -45,5 +55,27 @@ public class UserController {
             return new BasicRespone<>("Invalid User Info", -1, null);
 
         return userService.signUp(userInfo);
+    }
+
+    @PostMapping("/forgot_password")
+    public BasicRespone<String> processForgotPassword(HttpServletRequest request) {
+        try {
+            String email = request.getParameter("email");
+            String token = RandomString.make(55);
+
+            BasicRespone<User> userBasicRespone = userService.updateResetPassword(token, email);
+
+            if(userBasicRespone.getData() == null)
+                return new BasicRespone<>(userBasicRespone.getMessage(), -1, null);
+
+            String resetPasswordLink = Utility.getSiteURL(request) + "/reset_password?token=" + token;
+            emailUtil.sendEmail(email, resetPasswordLink);
+
+            return new BasicRespone<>("Send email success", 0, token);
+        } catch (UnsupportedEncodingException ex) {
+            return new BasicRespone<>(ex.getMessage(), -1, null);
+        } catch (MessagingException e) {
+            return new BasicRespone<>(e.getMessage(), -1, null);
+        }
     }
 }
