a4e985735ae93d10daee2138b26e3b87d204302e
thuhang2510 <tunguyet2510@gmail.com>
2022-12-03 20:21:33 +0700

    create interface UserRepo extends JpaRepository to  communicating with DB

---
+++ src/main/java/com/example/chattai/repositories/UserRepo.java
@@ -0,0 +1,10 @@
+package com.example.chattai.repositories;
+
+import com.example.chattai.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepo extends JpaRepository<User, String> {
+
+}
