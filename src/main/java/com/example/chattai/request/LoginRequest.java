fd8b901f84126f56e2f1670e0cfa172508e0f8d7
thuhang2510 <tunguyet2510@gmail.com>
2022-12-03 20:42:59 +0700

    add validation email in class LoginRequest

--- src/main/java/com/example/chattai/request/LoginRequest.java
+++ src/main/java/com/example/chattai/request/LoginRequest.java
@@ -1,9 +1,13 @@
 package com.example.chattai.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class LoginRequest {
+    @NotBlank(message = "Email cannot blank")
+    @Email(message = "Invalid email")
     private String email;
     private String password;
 }
