f364f5f0977d606e6f8f345ec290fa4f01ce1ac3
thuhang2510 <tunguyet2510@gmail.com>
2022-12-03 20:33:31 +0700

    add method addUser to create new user

--- src/main/java/com/example/chattai/services/UserService.java
+++ src/main/java/com/example/chattai/services/UserService.java
@@ -1,6 +1,8 @@
 package com.example.chattai.services;
 
+import com.example.chattai.model.User;
 import com.example.chattai.repositories.UserRepo;
+import com.example.chattai.respone.BasicRespone;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -8,4 +10,20 @@ import org.springframework.stereotype.Service;
 public class UserService {
     @Autowired
     private UserRepo userRepo;
+
+    public BasicRespone<User> addUser(User userInfo) {
+        try
+        {
+            User user = userRepo.save(userInfo);
+
+            if(user == null)
+                return new BasicRespone<User>("Add user failed", -1, null);
+
+            return new BasicRespone<User>("Add user success", 0, user);
+        }
+        catch (RuntimeException ex)
+        {
+            return new BasicRespone<User>("Exception Occured : " + ex.getMessage(), -1, null);
+        }
+    }
 }
