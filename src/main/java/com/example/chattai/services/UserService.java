24fa3b2ef45d6e9b6727f65664351fae7b3009df
thuhang2510 <tunguyet2510@gmail.com>
2022-12-03 20:30:51 +0700

    mark UserRepo of UserService by Annotation @Autowired

--- src/main/java/com/example/chattai/services/UserService.java
+++ src/main/java/com/example/chattai/services/UserService.java
@@ -1,7 +1,11 @@
 package com.example.chattai.services;
 
+import com.example.chattai.repositories.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+    @Autowired
+    private UserRepo userRepo;
 }
