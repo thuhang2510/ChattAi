c1e21d1100b1fb454b62016b9166100bd9e95050
thuhang2510 <tunguyet2510@gmail.com>
2022-12-03 20:04:22 +0700

    init project

---
+++ src/main/java/com/example/chattai/ChattAiApplication.java
@@ -0,0 +1,13 @@
+package com.example.chattai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChattAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChattAiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/chattai/ChattAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chattai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChattAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
