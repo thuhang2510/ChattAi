4acba46185b3be0511d776714cf1c7b81c4f0eeb
thuhang2510 <tunguyet2510@gmail.com>
2022-12-06 10:40:20 +0700

    add validation email

--- src/main/java/com/example/chattai/repositories/UserRepo.java
+++ src/main/java/com/example/chattai/repositories/UserRepo.java
@@ -1,11 +1,12 @@
 package com.example.chattai.repositories;
 
 import com.example.chattai.model.User;
+import jakarta.validation.constraints.Email;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepo extends JpaRepository<User, String> {
-    User getUserByEmailAndPassword(String email, String password);
-    User getUserByEmail(String email);
+    User getUserByEmailAndPassword(@Email String email, String password);
+    User getUserByEmail(@Email String email);
 }
